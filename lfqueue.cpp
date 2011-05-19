a535f293ead53c462c51c21d2c449c4ef07da248
Jeffrey Elliot Trull <linmodemstudent@gmail.com>
2011-05-19 14:47:43 -0500

    Add options to vary the number of producer and consumer threads.  Thread counts above 1 producer and 1 consumer disable value checking, as we cannot be sure which values each consumer will encounter.

--- lfqueue.cpp
+++ lfqueue.cpp
@@ -11,33 +11,66 @@
 #include <boost/thread.hpp>
 #include <boost/lockfree/fifo.hpp>
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 static const int opcount = 50000000;
 static boost::lockfree::fifo<int> queue;
 
-void producer() {
+static int num_pthreads;
+static int num_cthreads;
+
+void producer(int opcount) {
   for (int i = 0; i < opcount; ++i) {
     queue.enqueue(i);
   }
 }
 
-void consumer() {
+void consumer(int opcount) {
   for (int i = 0; i < opcount; ++i) {
     // spin until queue has contents
-    while (queue.empty()) {}
     int popval;
-    queue.dequeue(&popval);
-    assert(popval == i);
+    while (!queue.dequeue(&popval)) {}
+    if ((num_cthreads == 1) && (num_pthreads == 1)) {
+      std::cerr << "num_cthreads: " << num_cthreads << " num_pthreads: " << num_pthreads << std::endl;
+      assert(popval == i);
+    }
   }
 }
 
 int main(int argc, char **argv) {
-  // launch threads
-  boost::thread consthread(consumer);
-  boost::thread prodthread(producer);
+  po::options_description optionsdesc("Allowed Options:");
+  optionsdesc.add_options()
+    ("help", "Describe available options")
+    ("pthreads", po::value<int>(&num_pthreads)->default_value(1), "Number of producer threads")
+    ("cthreads", po::value<int>(&num_cthreads)->default_value(1), "Number of consumer threads");
 
-  // wait for both to finish
-  consthread.join();
-  prodthread.join();
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, optionsdesc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << optionsdesc << std::endl;
+    return 1;
+  }
+
+  // launch threads
+  std::vector<boost::shared_ptr<boost::thread> > prodthreads;
+  for (int i = 0; i < num_pthreads; ++i) {
+    prodthreads.push_back(boost::shared_ptr<boost::thread>(new boost::thread(producer, opcount / num_pthreads)));
+  }
+  std::vector<boost::shared_ptr<boost::thread> > consthreads;
+  for (int i = 0; i < num_cthreads; ++i) {
+    consthreads.push_back(boost::shared_ptr<boost::thread>(new boost::thread(consumer, opcount / num_cthreads)));
+  }
+
+  // wait for all threads to finish
+  for (int i = 0; i < num_cthreads; ++i) {
+    consthreads[i]->join();
+  }
+  for (int i = 0; i < num_pthreads; ++i) {
+    prodthreads[i]->join();
+  }
 
   // queue should now be empty
   assert(queue.empty());
--- tradqueue.cpp
+++ tradqueue.cpp
@@ -10,11 +10,17 @@
 #include <boost/thread.hpp>
 #include <boost/thread/mutex.hpp>
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
 static const int opcount = 50000000;
 static std::queue<int> queue;
 static boost::mutex shared_lock;
 
-void producer() {
+static int num_pthreads;
+static int num_cthreads;
+
+void producer(int opcount) {
   int produced_count = 0;
   while (produced_count < opcount) {
     // lock
@@ -24,14 +30,16 @@ void producer() {
   }
 }
 
-void consumer() {
+void consumer(int opcount) {
   int consumed_count = 0;
   while (consumed_count < opcount) {
     // lock
     boost::mutex::scoped_lock sl(shared_lock);
     if (!queue.empty()) {
       int front = queue.front();
-      assert(front == consumed_count);
+      if ((num_pthreads == 1) && (num_cthreads == 1)) {
+	assert(front == consumed_count);
+      }
       queue.pop();
       consumed_count++;
     }
@@ -40,13 +48,38 @@ void consumer() {
 }
 
 int main(int argc, char **argv) {
-  // launch threads
-  boost::thread consthread(consumer);
-  boost::thread prodthread(producer);
+  po::options_description optionsdesc("Allowed Options:");
+  optionsdesc.add_options()
+    ("help", "Describe available options")
+    ("pthreads", po::value<int>(&num_pthreads)->default_value(1), "Number of producer threads")
+    ("cthreads", po::value<int>(&num_cthreads)->default_value(1), "Number of consumer threads");
 
-  // wait for both to finish
-  consthread.join();
-  prodthread.join();
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, optionsdesc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << optionsdesc << std::endl;
+    return 1;
+  }
+
+  // launch threads
+  std::vector<boost::shared_ptr<boost::thread> > prodthreads;
+  for (int i = 0; i < num_pthreads; ++i) {
+    prodthreads.push_back(boost::shared_ptr<boost::thread>(new boost::thread(producer, opcount / num_pthreads)));
+  }
+  std::vector<boost::shared_ptr<boost::thread> > consthreads;
+  for (int i = 0; i < num_cthreads; ++i) {
+    consthreads.push_back(boost::shared_ptr<boost::thread>(new boost::thread(consumer, opcount / num_cthreads)));
+  }
+
+  // wait for all threads to finish
+  for (int i = 0; i < num_cthreads; ++i) {
+    consthreads[i]->join();
+  }
+  for (int i = 0; i < num_pthreads; ++i) {
+    prodthreads[i]->join();
+  }
 
   // queue should now be empty
   assert(queue.empty());
